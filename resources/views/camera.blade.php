1547a88b4aef1b7d4d8487f912d65e46817b9db4
YuugouOhno <yuugou.ohno@gmail.com>
2023-06-20 19:12:57 +0900

    リンクをクリックしたい、、できない

--- resources/views/camera.blade.php
+++ resources/views/camera.blade.php
@@ -13,20 +13,13 @@
     <a-scene embedded arjs="debugUIEnabled:false; patternRatio:0.70">
 
         <a-marker type="pattern" url="{{asset('marker/pattern2.patt')}}">
-
             <!-- ARmarker -->
             <!-- <a-entity geometry="primitive: plane; height: 1; width: 1" position="0 0 0" material="side: double; opacity:0.5;" rotation="90 0 0"></a-entity> -->
 
-            <!-- マウスカーソル -->
-            <!-- <a-entity id="mouseCursor" cursor="rayOrigin: mouse" raycaster="objects: .raycastable"></a-entity> -->
-            <!-- 当たり判定オブジェクト -->
-            <!-- <a-entity position="0 -0.05 0" hit-box>
-                <a-box></a-box>
-            </a-entity> -->
+            <a-link href="https://aframe.io/docs/1.4.0/primitives/a-link.html" title="test" src="https://aframe.io/docs/1.4.0/primitives/a-link.html"></a-link>
+            <!-- <a-image src="{{ $card->ar_icon_image }}" height="1.35" width="1.35" position="-0.55 0 0" rotation="270 0 0" scale="1 1 1" side="double"></a-image>
 
-            <a-image src="{{ $card->ar_icon_image }}" height="1.35" width="1.35" position="-0.55 0 0" rotation="270 0 0" scale="1 1 1" side="double"></a-image>
-
-            <a-text value="{{ $card->ar_name }}" side="double" position="0.2 0 0" rotation="270 0 0" align="left" scale="2 2 2"></a-text>
+            <a-text value="{{ $card->ar_name }}" side="double" position="0.4 0 0" rotation="270 0 0" align="left" scale="2 2 2"></a-text>
 
             <a-text value="{{ $card->ar_text }}" side="double" position="0.7 0 1.5" rotation="270 0 0" align="right" scale="1.3 1.3 1.3"></a-text>
 
@@ -35,19 +28,13 @@
             <a-image src="{{asset('ar/Twitter.png')}}" height="1" width="1" position="-0.7 0 3" rotation="270 0 0" scale="1 1 1" side="double"></a-image>
 
             <a-image src="{{asset('ar/Github.png')}}" height="1" width="1" position="0.85 0 3" rotation="270 0 0" scale="1 1 1" side="double"></a-image>
-            <a-image src="{{asset('ar/User.png')}}" height="1" width="1" position="2.3 0 3" rotation="270 0 0" scale="1 1 1" side="double"></a-image>
+            <a-image src="{{asset('ar/User.png')}}" height="1" width="1" position="2.3 0 3" rotation="270 0 0" scale="1 1 1" side="double"></a-image> -->
         </a-marker>
 
         <a-entity camera></a-entity>
     </a-scene>
     <script>
-        AFRAME.registerComponent('hit-box', {
-            init: function() {
-                this.el.addEventListener('click', () => {
-                    alert('clicked!');
-                });
-            }
-        });
+        
     </script>
 </body>
 
