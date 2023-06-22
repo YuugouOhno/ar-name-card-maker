e0bde708bb3247e0abeec4b5805a61ecb28c7e76
YuugouOhno <yuugou.ohno@gmail.com>
2023-06-22 16:13:28 +0900

    ar card

--- resources/views/camera.blade.php
+++ resources/views/camera.blade.php
@@ -16,8 +16,7 @@
             <!-- ARmarker -->
             <!-- <a-entity geometry="primitive: plane; height: 1; width: 1" position="0 0 0" material="side: double; opacity:0.5;" rotation="90 0 0"></a-entity> -->
 
-            <a-link href="https://aframe.io/docs/1.4.0/primitives/a-link.html" title="test" src="https://aframe.io/docs/1.4.0/primitives/a-link.html"></a-link>
-            <!-- <a-image src="{{ $card->ar_icon_image }}" height="1.35" width="1.35" position="-0.55 0 0" rotation="270 0 0" scale="1 1 1" side="double"></a-image>
+            <a-image src="{{ $card->ar_icon_image }}" height="1.35" width="1.35" position="-0.55 0 0" rotation="270 0 0" scale="1 1 1" side="double"></a-image>
 
             <a-text value="{{ $card->ar_name }}" side="double" position="0.4 0 0" rotation="270 0 0" align="left" scale="2 2 2"></a-text>
 
@@ -28,7 +27,7 @@
             <a-image src="{{asset('ar/Twitter.png')}}" height="1" width="1" position="-0.7 0 3" rotation="270 0 0" scale="1 1 1" side="double"></a-image>
 
             <a-image src="{{asset('ar/Github.png')}}" height="1" width="1" position="0.85 0 3" rotation="270 0 0" scale="1 1 1" side="double"></a-image>
-            <a-image src="{{asset('ar/User.png')}}" height="1" width="1" position="2.3 0 3" rotation="270 0 0" scale="1 1 1" side="double"></a-image> -->
+            <a-image src="{{asset('ar/User.png')}}" height="1" width="1" position="2.3 0 3" rotation="270 0 0" scale="1 1 1" side="double"></a-image>
         </a-marker>
 
         <a-entity camera></a-entity>
