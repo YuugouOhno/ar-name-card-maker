6d4e8e92a802a51e35e303388d3378d9fd8e35a7
YuugouOhno <yuugou.ohno@gmail.com>
2023-06-23 10:51:20 +0900

    フロント

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,11 +6,12 @@
         <meta name="csrf-token" content="{{ csrf_token() }}" />
 
         <title inertia>AR名刺メーカー</title>
+        <meta name="description" content="僕の私の準最強のAR名刺メーカーです。誰でもAR名刺が作れます(？)">
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-
+        <link rel="shortcut icon" href="{{ asset('/favicon.ico') }}">
         <!-- Scripts -->
         @routes
         @viteReactRefresh
